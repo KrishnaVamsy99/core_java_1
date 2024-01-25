49623a7d7fd6cdcfc757a88e7bbea5066fc4d0f6
krishna <gvkvamsy99@gmail.com>
2024-01-25 17:31:56 +0530

    Added new Files

--- CenteredSquare.java
+++ CenteredSquare.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-class Hogben 
+class CenteredSquare
 {
     public static int myFun(int n)
     {
@@ -8,14 +8,14 @@ class Hogben
         {
             return 1;
         }
-        return myFun(n-1) + 2 *(n-1);
+        return myFun(n-1) + 4 *(n-1);
     }
     public static void main(String[] args)
     {
         Scanner s = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int n = s.nextInt();
-        int m = Hogben.myFun(n);
+        int m = CenteredSquare.myFun(n);
         System.out.println(m);
         s.close();
     }
